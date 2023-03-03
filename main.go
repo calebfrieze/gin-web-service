ac5bfd770ccd04858edd90c7e00d7dd32d3fc5a3
Caleb Frieze <calebfrieze@gmail.com>
2023-03-03 13:33:44 -0700

    add handler for getting album by id

--- main.go
+++ main.go
@@ -23,6 +23,18 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
+}
+
 func createAlbum(c *gin.Context) {
 	var newAlbum album
 
@@ -38,6 +50,7 @@ func createAlbum(c *gin.Context) {
 func main() {
 	router := gin.Default()
 
+	router.GET("/album/:id", getAlbumByID)
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", createAlbum)
 
